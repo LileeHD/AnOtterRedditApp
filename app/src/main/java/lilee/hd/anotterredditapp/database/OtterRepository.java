ec4f4fd40e2e56af46360b0368e7fbea1c94f29c
LileeHD <lileetoile@gmail.com>
2019-12-20 01:56:34 +0100

    In Progress: viewmodel+livedata+room+recyclerview Can't display data

--- app/src/main/java/lilee/hd/anotterredditapp/database/OtterRepository.java
+++ app/src/main/java/lilee/hd/anotterredditapp/database/OtterRepository.java
@@ -55,42 +55,4 @@ public class OtterRepository {
         data.setValue(dataSet);
         return data;
     }
-    public void redditCall() {
-        final MutableLiveData<Feed> stationList = new MutableLiveData<>();
-        Call<Feed> call = redditAPI.getHomeFeed();
-        call.enqueue(new Callback<Feed>() {
-            @Override
-            public void onResponse(Call<Feed> call, Response<Feed> response) {
-                Log.d(TAG_TOKEN, "onResponse: Server Response" + response.toString());
-                Log.d(TAG_TOKEN, "onResponse: received information" + response.body().toString());
-                if (response.isSuccessful()){
-                    response.body().getData().getChildren().toString();
-                    ArrayList<Children> childrenArrayList = response.body().getData().getChildren();
-                    for (int i = 0; i < childrenArrayList.size(); i++) {
-//                    Post info = childrenArrayList.get(i).getData();
-//                    String title = info.getTitle();
-                        Log.d(TAG_TOKEN, "onResponse: \n" +
-                                "kind: " + childrenArrayList.get(i).getKind() + "\n" +
-                                "subreddit_name_prefixed: " + childrenArrayList.get(i).getData().getSubredditR() + "\n" +
-                                "author: " + childrenArrayList.get(i).getData().getAuthor() + "\n" +
-                                "created: " + childrenArrayList.get(i).getData().getDate() + "\n" +
-                                "title: " + childrenArrayList.get(i).getData().getTitle() + "\n" +
-                                "selftext: " + childrenArrayList.get(i).getData().getBody() + "\n" +
-                                "url: " + childrenArrayList.get(i).getData().getPostUrl() + "\n" +
-                                "ups: " + childrenArrayList.get(i).getData().getUps() + "\n" +
-                                "thumbnail: " + childrenArrayList.get(i).getData().getThumbnail() + "\n" +
-                                "reddit_video: " + childrenArrayList.get(i).getData().getVideoUrl() + "\n" +
-                                "-------------------------------------------------------------------------\n\n");
-                        Log.d(TAG, "onResponse: redditCall");
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Feed> call, Throwable t) {
-                Log.e(TAG_TOKEN, "onFailure: ERROR: " + t.getMessage());
-//                Toast.makeText(context, "Something wrong", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
 }
